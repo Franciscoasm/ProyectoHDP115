879fbc6478860d81849458806f1243238a9bd550
Francisco Mendez <alexadrmendez@gmal.com>
2020-07-01 02:55:03 -0600

    Actualizacion estrusctura

--- ardc/agregar/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgregarConfig(AppConfig):
-    name = 'agregar'
--- ardc/agregar/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Departamento
-from .models import Municipio
-
-# Create your views here.
-def agregar(request):
-    departamentos = Departamento.objects.all()
-    municipios = Municipio.objects.all()
-    return render(request, "agregar/agregar.html",{'departamentos':departamentos, 'municipios':municipios})
